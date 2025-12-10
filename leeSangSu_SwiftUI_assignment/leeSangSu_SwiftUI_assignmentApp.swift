39af4c0e351c24703597aba3dee1ba223ef27a08
soo127 <ss_127@naver.com>
2025-12-11 02:33:05 +0900

    feat: #1 진입점을 메인 탭뷰로

---
+++ leeSangSu_SwiftUI_assignment/leeSangSu_SwiftUI_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  leeSangSu_SwiftUI_assignmentApp.swift
+//  leeSangSu_SwiftUI_assignment
+//
+//  Created by 이상수 on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct leeSangSu_SwiftUI_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainTabView()
+        }
+    }
+}

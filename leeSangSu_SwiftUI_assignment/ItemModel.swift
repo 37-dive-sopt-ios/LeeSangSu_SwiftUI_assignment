690141c4d693a6cd21f445b4132520f295ad2565
soo127 <ss_127@naver.com>
2025-12-11 02:32:31 +0900

    feat: #1 공통 모델/뷰

---
+++ leeSangSu_SwiftUI_assignment/ItemModel.swift
@@ -0,0 +1,14 @@
+//
+//  ItemModel.swift
+//  leeSangSu_SwiftUI_assignment
+//
+//  Created by 이상수 on 12/10/25.
+//
+
+import Foundation
+
+struct ItemModel: Identifiable {
+    let id = UUID()
+    let imageName: String
+    let title: String
+}

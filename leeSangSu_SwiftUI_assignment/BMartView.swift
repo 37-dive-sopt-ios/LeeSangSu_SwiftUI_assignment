31f7364fc32c293c4b1010cf307fab355b1cb07d
soo127 <ss_127@naver.com>
2025-12-11 02:31:03 +0900

    feat: #1 비마트 영역 뷰

---
+++ leeSangSu_SwiftUI_assignment/BMartView.swift
@@ -0,0 +1,19 @@
+//
+//  BMartView.swift
+//  leeSangSu_SwiftUI_assignment
+//
+//  Created by 이상수 on 12/10/25.
+//
+
+import SwiftUI
+
+struct BMartView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Image("b_mart_img")
+            Text("전 상품 쿠폰팩 + 60%특가 >")
+                .font(.system(size: 14, weight: .bold))
+            
+        }
+    }
+}
